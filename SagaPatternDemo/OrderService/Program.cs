be283c72c97fb17c0de77ddc3be195ea5b87bfbe
NicholasPilotto <nicholaspilotto@gmail.com>
2025-05-24 15:07:27 +0200

    Removed comments

--- SagaPatternDemo/OrderService/Program.cs
+++ SagaPatternDemo/OrderService/Program.cs
@@ -36,7 +36,6 @@ builder.Services.AddMassTransit(busConfigurator =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
